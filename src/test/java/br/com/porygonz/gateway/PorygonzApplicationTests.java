2aa5d686245a528ff55651367ef6982f0c85017a
danielsanoli <daniel.sanoli28@gmail.com>
2025-06-12 23:29:00 -0300

    inicial commit

---
+++ src/test/java/br/com/porygonz/gateway/PorygonzApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.porygonz.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorygonzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
